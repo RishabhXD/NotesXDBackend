7e09f466bdc4ad2bfeb307c45fc27204a83d4604
Rishabh Pant <81542717+RishabhXD@users.noreply.github.com>
2022-05-29 03:29:29 +0530

    mongouri

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoURI = `mongodb://localhost:27017/NotesXD?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false`;
+const mongoURI = process.env.MONGOURI;
 
 const connectToMongo = () => {
   mongoose.connect(mongoURI, () => {
